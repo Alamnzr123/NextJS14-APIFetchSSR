e6106458885bc7b99a63eb7d70cc6b16b142c5e2
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2023-11-22 11:45:04 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  env: {
+    NEXT_API: 'jsonplaceholder.typicode.com/users',
+  },
 }
 
 module.exports = nextConfig
